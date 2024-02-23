50c618b2291d6504fe1c78015235d1d93dc6e8c2
Hodorsun <146957724+Hodorsun@users.noreply.github.com>
2024-02-23 22:52:20 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="app">
+       <post-form
+                  @create="createPost"/>
+       <post-list 
+                 :posts="posts"/>
+    </div>
+</template>
+
+<script>
+    import PostForm from '@/components/PostForm';
+    import PostList from '@/components/PostList';
+    export default {
+        components: {
+            PostList, PostForm
+        },
+        data() {
+            return {
+                posts: [
+                    { id: 1, title: 'Javascript', body: 'description of the post' },
+                    { id: 2, title: 'Javascript 2', body: 'description of the post 2' },
+                    { id: 3, title: 'Javascript 3', body: 'description of the post 3' },
+                ],
+            }
+        },
+        methods: {
+            createPost(post) {
+                this.posts.push(post);
+            }
+        }
+    }
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+} 
+
+.app{
+    padding: 20px;
+}
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post" v-for="post in posts">
+        <div><strong>Name:</strong> {{ post.title }}</div>
+        <div><strong>Description:</strong> {{ post.body }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            posts: {
+                type: Array,
+                requared: true
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
